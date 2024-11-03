c242755c1dc16d56e76e339f802c65ae91999e0a
= <=>
2024-11-03 01:17:05 -0800

    tutorial 20 completed

---
+++ Tu_19_Dictonary.py
@@ -0,0 +1,15 @@
+# Dictonary in python
+
+detailOne = {'name': 'Aman', 'age': '15 years', 'status': 'learning_Python'}
+detailTwo = {100: 3, 200: 5, 300: 6}
+
+#-------> Dictonary methods
+
+# detailOne.update(detailTwo) ---> It merge the two dictonary and update the firstOne
+# detailOne.clear() ---> It clear dictonary
+# detailOne.pop('age') ---> It remove a particular given element from the dictonary
+# detailOne.popitem()  ---> It remove the last element from the dictonary
+
+
+print(detailOne) 
+ 
---
+++ Tu_20_forLoop_with_else.py
@@ -0,0 +1,23 @@
+
+
+# for i in range (1, 11) : 
+#     print(i)
+    
+#     if(i== 4) : 
+#         break
+
+# else :  
+#   print("I is not available...")
+
+
+
+n = 0
+while n <= 10 : 
+    print(n)
+    n  = n + 1
+    
+    if(n ==4) : 
+        break
+
+else : 
+    print("I is not available..")
